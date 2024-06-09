48972acf8d64dea1562962289252e3dea3e202dd
kscheufe <90880062+kscheufe@users.noreply.github.com>
2024-06-08 20:01:45 -0400

    additional, faster solution for Python InsertDeleteGetRandom completed

--- TopInterview150/InsertDeleteGetRandom/solution.py
+++ TopInterview150/InsertDeleteGetRandom/solution.py
@@ -1,12 +1,64 @@
 class RandomizedSet(object):
     
     def __init__(self):
-        self.thisSet = set()
+        self.lst = []
+        self.idxMap = {}
+    def search(self, val):
+        return val in self.idxMap
+    
     def insert(self, val):
         """
         :type val: int
         :rtype: bool
         """
+        """if already in the map, return false"""
+        if self.search(val):
+            return False
+        """if not in the map, add it and return true"""
+        self.lst.append(val)
+        self.idxMap[val] = len(self.lst) - 1
+        return True      
+
+    def remove(self, val):
+        """
+        :type val: int
+        :rtype: bool
+        """
+        """if not in the list, return false"""
+        if not self.search(val):
+            return False
+        """if in the list, remove it and return true"""
+        idx = self.idxMap[val]
+        self.lst[idx] = self.lst[-1]
+        self.idxMap[self.lst[-1]] = idx
+        self.lst.pop()
+        del self.idxMap[val]
+        return True
+        
+
+    def getRandom(self):
+        """
+        :rtype: int
+        """
+        return random.choice(self.lst)
+        
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
+
+"""
+class RandomizedSet(object):
+    
+    def __init__(self):
+        self.thisSet = set()
+    def insert(self, val):
+        ""
+        :type val: int
+        :rtype: bool
+        ""
         if val in self.thisSet:
             return False
         self.thisSet.add(val)
@@ -14,10 +66,10 @@ class RandomizedSet(object):
         
 
     def remove(self, val):
-        """
+        ""
         :type val: int
         :rtype: bool
-        """
+        ""
         if val not in self.thisSet:
              return False
         self.thisSet.remove(val)
@@ -26,9 +78,9 @@ class RandomizedSet(object):
         
 
     def getRandom(self):
-        """
+        ""
         :rtype: int
-        """
+        ""
         return random.choice(list(self.thisSet))
 
 # Your RandomizedSet object will be instantiated and called as such:
@@ -36,3 +88,4 @@ class RandomizedSet(object):
 # param_1 = obj.insert(val)
 # param_2 = obj.remove(val)
 # param_3 = obj.getRandom()
+"""
