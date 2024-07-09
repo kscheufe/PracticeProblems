2befa14bebc460fdd625f07eccd5c17c9a0ca355
kscheufe <90880062+kscheufe@users.noreply.github.com>
2024-07-09 00:08:36 -0400

    C++ solution for length of last word

---
+++ TopInterview150/LengthOfLastWord/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        bool start = false;
+        int num = 0;
+        for (int i = s.size()-1; i >=0; i--)
+        {
+            if (s[i] != ' ')
+            {
+                num++;
+                start = true;
+            }
+            else if (start)
+            {
+                return num;
+            }
+        }
+        return num;
+    }
+};
