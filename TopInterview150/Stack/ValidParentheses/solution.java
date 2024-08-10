f77d22fc5025ec7ef88e4e0470654675fbe5bf2d
kscheufe <90880062+kscheufe@users.noreply.github.com>
2024-08-10 15:57:24 -0400

    Cleaner, faster Java Solution for Valid parentheses

--- TopInterview150/Stack/ValidParentheses/solution.java
+++ TopInterview150/Stack/ValidParentheses/solution.java
@@ -28,6 +28,26 @@ Constraints:
 1 <= s.length <= 10^4
 s consists of parentheses only '()[]{}'.
  */
+
+
+ class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (char c : s.toCharArray())
+        {
+            if (c == '[')
+                stack.push(']');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '(')
+                stack.push(')');
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
+        }
+        return stack.isEmpty();
+    }
+ }
+ /*
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
@@ -63,3 +83,4 @@ class Solution {
         return false;
     }
 }
+     */
