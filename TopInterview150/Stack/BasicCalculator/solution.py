bb3abd54b49060e142468ac6548fd7cab774173a
kscheufe <90880062+kscheufe@users.noreply.github.com>
2024-08-20 16:22:34 -0400

    py Solution for Python

---
+++ TopInterview150/Stack/BasicCalculator/solution.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def calculate(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        num = 0
+        signValue = 1
+        result = 0
+        stack = []
+
+        for char in s:
+            if char.isdigit():
+                num = num * 10 + int(char)
+            elif char == '+' or char == '-':
+                result += num*signValue
+                signValue = -1 if char == '-' else 1
+                num = 0
+            elif char == '(':
+                stack.append(result)
+                stack.append(signValue)
+                result = 0
+                signValue = 1
+            elif char == ')':
+                result += signValue*num
+                result *= stack.pop()
+                result += stack.pop()
+                num = 0
+        return result + num*signValue
