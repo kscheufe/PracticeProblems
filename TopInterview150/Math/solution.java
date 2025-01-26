1758af634bfbc1dfc5281c7c1844c0f36e40f8c3
kscheufe <90880062+kscheufe@users.noreply.github.com>
2025-01-25 20:07:58 -0500

    Faster/Follow up solution for Java numerical isPalindrome

--- TopInterview150/Math/solution.java
+++ TopInterview150/Math/solution.java
@@ -32,12 +32,22 @@ Follow up: Could you solve it without converting the integer to a string?
 
  class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0) return false; //logically shouldn't be the case, but this question has silly parameters
+        if (x < 0 || x%10 == 0 && x!=0) return false;
+        int reversed = 0;
+        while (x > reversed)
+        {
+            reversed = reversed*10 + x%10;
+            x/=10;
+        }
+        return x==reversed || x==reversed/10;
+
+        
+        /*if (x < 0) return false; //logically shouldn't be the case, but this question has silly parameters
         String num = Integer.toString(x);
         for (int i = 0, j = num.length()-1; i < j; i++, j--)
         {
             if (num.charAt(i) != num.charAt(j)) return false;
         }
-        return true;
+        return true;*/
     }
 }
