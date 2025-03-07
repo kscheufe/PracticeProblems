ee71086c8faee0e9fcd974d9956d466e05cbcecc
kscheufe <90880062+kscheufe@users.noreply.github.com>
2025-03-06 16:58:40 -0800

    Second SQL Solution for Product Price at a Given Date

--- SQL50/AdvancedSelectAndJoins/ProductPriceAtAGivenDate.sql
+++ SQL50/AdvancedSelectAndJoins/ProductPriceAtAGivenDate.sql
@@ -46,6 +46,30 @@ Output:
 | 3          | 10    |
 +------------+-------+
 */
+# schneller losung
+select 
+    distinct product_id, 
+    10 as price 
+from Products 
+where product_id not in(
+    select distinct product_id 
+    from Products 
+    where change_date <='2019-08-16' 
+)
+union 
+select 
+    product_id, 
+    new_price as price 
+from Products 
+where (product_id, change_date) in (
+    select 
+        product_id, 
+        max(change_date) as date 
+    from Products 
+    where change_date <='2019-08-16' 
+    group by product_id
+)
+
 
 # ersten losung
 with temp as (
