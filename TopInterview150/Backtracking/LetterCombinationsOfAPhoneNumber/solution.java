adf83e8f053e5026e2356da0a7529c0dfef6fb65
kscheufe <90880062+kscheufe@users.noreply.github.com>
2025-03-20 14:12:46 -0700

    Recursive Backtracking Solution for LetterCombinationsOfAPhoneNumber in Java

--- TopInterview150/Backtracking/LetterCombinationsOfAPhoneNumber/solution.java
+++ TopInterview150/Backtracking/LetterCombinationsOfAPhoneNumber/solution.java
@@ -33,36 +33,69 @@ import java.util.*;
 
 class Solution {
     public List<String> letterCombinations(String digits) {
-        if (digits.length() == 0) return new ArrayList<>();
-        
-        List<String> ret = new ArrayList<>();
+        List<String> result = new ArrayList<>();
+        if (digits.length()==0 || digits=="") return result;
         Map<Character, String> map = new HashMap<>();
-        Queue<String> queue = new LinkedList<>();
-        queue.offer("");
-        map.put('2', "abc");
-        map.put('3', "def");
-        map.put('4', "ghi");
-        map.put('5', "jkl");
-        map.put('6', "mno");
-        map.put('7', "pqrs");
-        map.put('8', "tuv");
-        map.put('9', "wxyz");
+            map.put('2', "abc");
+            map.put('3', "def");
+            map.put('4', "ghi");
+            map.put('5', "jkl");
+            map.put('6', "mno");
+            map.put('7', "pqrs");
+            map.put('8', "tuv");
+            map.put('9', "wxyz");
+        recurse(digits, 0, new StringBuilder(), result, map);
+        return result;
 
-
-        for (int i = 0; i < digits.length(); i++)
-        {
-            String newDigits = map.get(digits.charAt(i));
-            while (queue.peek().length() == i)
-            {
-                String oldString = queue.poll();
-                for (int j = 0; j < newDigits.length(); j++)
-                {
-                    queue.offer(oldString + newDigits.charAt(j));
-                }
-            }
-        }
-
-        while (queue.peek() != null) ret.add(queue.poll());
-        return ret;
     }
+
+    public void recurse(String digits, int index, StringBuilder comb, List<String> result, Map<Character, String> map) {
+        if (index == digits.length()) {//base case, when max depth has been reached
+            result.add(comb.toString());
+            return;
+        }     
+        
+        //convert string of letters for current number to a character array
+        char[] letters = map.get(digits.charAt(index)).toCharArray();
+
+        for (char c: letters) {
+            comb.append(c);//add current character
+            recurse(digits, index + 1, comb, result, map);//recurse to next level
+            comb.deleteCharAt(comb.length()-1);//delete most recent char from string for next option
+        }
+    }
+
+    // public List<String> letterCombinations(String digits) {
+    //     if (digits.length() == 0) return new ArrayList<>();
+
+    //     List<String> ret = new ArrayList<>();
+    //     Map<Character, String> map = new HashMap<>();
+    //     Queue<String> queue = new LinkedList<>();
+    //     queue.offer("");
+    //     map.put('2', "abc");
+    //     map.put('3', "def");
+    //     map.put('4', "ghi");
+    //     map.put('5', "jkl");
+    //     map.put('6', "mno");
+    //     map.put('7', "pqrs");
+    //     map.put('8', "tuv");
+    //     map.put('9', "wxyz");
+
+
+    //     for (int i = 0; i < digits.length(); i++)
+    //     {
+    //         String newDigits = map.get(digits.charAt(i));
+    //         while (queue.peek().length() == i)
+    //         {
+    //             String oldString = queue.poll();
+    //             for (int j = 0; j < newDigits.length(); j++)
+    //             {
+    //                 queue.offer(oldString + newDigits.charAt(j));
+    //             }
+    //         }
+    //     }
+
+    //     while (queue.peek() != null) ret.add(queue.poll());
+    //     return ret;
+    // }
 }
