1d82e0c7fd58f15afb4c0ad5254ada64da1be06a
kscheufe <90880062+kscheufe@users.noreply.github.com>
2025-04-18 23:58:41 -0700

    C++ Solution for findminimuminrotatedsortedarray, from command line function

---
+++ TopInterview150/BinarySearch/FindMinimumInRotatedSortedArray/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    public:
+        int findMin(vector<int>& nums) {
+            if (nums.size()==1) return nums[0];
+            int l = 0;
+            int r = nums.size()-1;
+            if (nums[l] > nums[l+1]) return nums[l+1];
+            if (nums[r] < nums[r-1]) return nums[r];
+
+            while (l <= r) {
+                int m = l+(r-l)/2;
+
+                if (m == 0 || nums[m] < nums[m-1])
+                    return nums[m];
+
+                if (nums[m] > nums[l]) l = m+1;
+                else r = m-1;
+            }
+
+            return nums[r];
+
+        }
+    };
