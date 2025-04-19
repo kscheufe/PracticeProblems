43be2b767bf7feac07f36f5a77ca1b294835838a
kscheufe <90880062+kscheufe@users.noreply.github.com>
2025-04-19 00:16:17 -0700

    Java Solution for findminimuminrotatedsortedarray, from command line function

--- TopInterview150/BinarySearch/FindMinimumInRotatedSortedArray/solution.java
+++ TopInterview150/BinarySearch/FindMinimumInRotatedSortedArray/solution.java
@@ -40,6 +40,27 @@ n == nums.length
 All the integers of nums are unique.
 nums is sorted and rotated between 1 and n times.
  */
+/* Optimal Solution? */
+class Solution {
+    public int findMin(int[] nums) {
+        int l = 0;
+        int r = nums.length-1;
+
+        if (nums[l] <= nums[r]) return nums[l];
+
+        while (l <= r) {
+            int mid = l + (r-l)/2;
+
+            if (mid > 0 && nums[mid] < nums[mid-1]) return nums[mid];
+            if (mid < nums.length-1 && nums[mid] > nums[mid+1]) return nums[mid+1];
+
+            if (nums[mid] >= nums[l]) l = mid + 1;
+            else r = mid -1;
+        }
+        return -1;
+    }
+}
+/**
 class Solution {
     public int findMin(int[] nums) {
         int l = 0;
@@ -66,4 +87,4 @@ class Solution {
         }
         return nums[r];
     }
-}
+} //*/
